d40b10799583d786ffac3563fa12ea584faa6cba
krishna <krishnakarki1996@gmail.com>
2024-07-21 23:02:23 +0545

    create main function to implement addition

--- PyTest/main.py
+++ PyTest/main.py
@@ -1,1 +1,13 @@
+from add_function import addition
 
+print("Addition")
+print("Press q to quit the program")
+while True:
+    first = input("Enter first number: ")
+    if first.lower() == "q":
+        break
+    second = input("Enter second number: ")
+    if second.lower() == "q":
+        break
+    result = addition(first, second)
+    print(result)
