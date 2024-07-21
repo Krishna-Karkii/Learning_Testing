bb2a69d24b74b5d70b95752708258c631bb3df99
krishna <krishnakarki1996@gmail.com>
2024-07-21 23:01:04 +0545

    create function to add two numbers

--- PyTest/add_function.py
+++ PyTest/add_function.py
@@ -1,1 +1,13 @@
+def addition(num1, num2):
+    """This function gets two number as string converts them in integers
+    and returns the output by adding them"""
+    try:
+        result = int(num1) + int(num2)
+        return result
 
+    except ValueError:
+        print("Please enter a number.")
+
+
+if __name__ == "__main__":
+    print(addition(2, 3))
