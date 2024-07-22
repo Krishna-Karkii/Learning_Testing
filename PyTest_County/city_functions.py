01d17830ebc339e69653d4ff3da40b169e5bc263
krishna <krishnakarki1996@gmail.com>
2024-07-22 15:18:56 +0545

    implement get_city_info function

--- PyTest_County/city_functions.py
+++ PyTest_County/city_functions.py
@@ -1,1 +1,8 @@
+def get_city_info(city_name, country_name):
 
+    """Get city and country name, join them, and return the titled
+    version"""
+
+    result = f"{city_name}, {country_name}"
+
+    return result.title()
