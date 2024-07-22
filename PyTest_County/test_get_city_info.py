2da841cc79fd082989a82cafce389b19ea7e4bc0
krishna <krishnakarki1996@gmail.com>
2024-07-22 16:13:15 +0545

    implement testing with population

--- PyTest_County/test_get_city_info.py
+++ PyTest_County/test_get_city_info.py
@@ -4,3 +4,8 @@ from city_functions import get_city_info
 def test_city_country_info():
     result = get_city_info("kathmandu", "nepal")
     assert result == "Kathmandu, Nepal"
+
+
+def test_city_country_population_info():
+    result = get_city_info("kathmandu", "nepal", "30000000")
+    assert result == "Kathmandu, Nepal - Population = 30000000"
