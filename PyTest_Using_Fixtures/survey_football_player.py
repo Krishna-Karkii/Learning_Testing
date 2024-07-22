5f6e9fd16ed36f8bb290769b6b76aee920f68137
krishna <krishnakarki1996@gmail.com>
2024-07-22 18:32:18 +0545

    add store and get list method

--- PyTest_Using_Fixtures/survey_football_player.py
+++ PyTest_Using_Fixtures/survey_football_player.py
@@ -6,4 +6,9 @@ class FavoritePlayer:
     def get_question(self):
         return self.question
 
+    def store(self, response):
+        self.result.append(response)
+
+    def get_list(self):
+        return self.result
 
